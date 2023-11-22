b72427a10634afcd16737cb6729c20af945083eb
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-11-22 11:51:01 -0300

    Incompleto

--- WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -28,26 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.txbInput = new System.Windows.Forms.TextBox();
+            this.txbNome = new System.Windows.Forms.TextBox();
             this.btnSalvar = new System.Windows.Forms.Button();
             this.btnLer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbTelefone = new System.Windows.Forms.TextBox();
+            this.rbTelefone = new System.Windows.Forms.RadioButton();
+            this.rbEmail = new System.Windows.Forms.RadioButton();
+            this.cbxNome = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblTelefone = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // txbInput
+            // txbNome
             // 
-            this.txbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txbInput.Location = new System.Drawing.Point(13, 13);
-            this.txbInput.Name = "txbInput";
-            this.txbInput.Size = new System.Drawing.Size(186, 30);
-            this.txbInput.TabIndex = 0;
+            this.txbNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNome.Location = new System.Drawing.Point(13, 32);
+            this.txbNome.Name = "txbNome";
+            this.txbNome.Size = new System.Drawing.Size(186, 30);
+            this.txbNome.TabIndex = 0;
             // 
             // btnSalvar
             // 
             this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalvar.Location = new System.Drawing.Point(12, 323);
+            this.btnSalvar.Location = new System.Drawing.Point(12, 336);
             this.btnSalvar.Name = "btnSalvar";
             this.btnSalvar.Size = new System.Drawing.Size(89, 38);
-            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.TabIndex = 4;
             this.btnSalvar.Text = "Salvar";
             this.btnSalvar.UseVisualStyleBackColor = true;
             this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
@@ -55,25 +70,184 @@
             // btnLer
             // 
             this.btnLer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLer.Location = new System.Drawing.Point(108, 323);
+            this.btnLer.Location = new System.Drawing.Point(108, 336);
             this.btnLer.Name = "btnLer";
             this.btnLer.Size = new System.Drawing.Size(91, 38);
-            this.btnLer.TabIndex = 2;
+            this.btnLer.TabIndex = 5;
             this.btnLer.Text = "Ler";
             this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Email";
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmail.Location = new System.Drawing.Point(13, 166);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(186, 30);
+            this.txbEmail.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Telefone";
+            // 
+            // txbTelefone
+            // 
+            this.txbTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTelefone.Location = new System.Drawing.Point(13, 99);
+            this.txbTelefone.Name = "txbTelefone";
+            this.txbTelefone.Size = new System.Drawing.Size(186, 30);
+            this.txbTelefone.TabIndex = 1;
+            // 
+            // rbTelefone
+            // 
+            this.rbTelefone.AutoSize = true;
+            this.rbTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbTelefone.Location = new System.Drawing.Point(205, 32);
+            this.rbTelefone.Name = "rbTelefone";
+            this.rbTelefone.Size = new System.Drawing.Size(82, 21);
+            this.rbTelefone.TabIndex = 4;
+            this.rbTelefone.TabStop = true;
+            this.rbTelefone.Text = "Telefone";
+            this.rbTelefone.UseVisualStyleBackColor = true;
+            // 
+            // rbEmail
+            // 
+            this.rbEmail.AutoSize = true;
+            this.rbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbEmail.Location = new System.Drawing.Point(205, 56);
+            this.rbEmail.Name = "rbEmail";
+            this.rbEmail.Size = new System.Drawing.Size(60, 21);
+            this.rbEmail.TabIndex = 9;
+            this.rbEmail.TabStop = true;
+            this.rbEmail.Text = "Email";
+            this.rbEmail.UseVisualStyleBackColor = true;
+            // 
+            // cbxNome
+            // 
+            this.cbxNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxNome.FormattingEnabled = true;
+            this.cbxNome.Location = new System.Drawing.Point(205, 99);
+            this.cbxNome.Name = "cbxNome";
+            this.cbxNome.Size = new System.Drawing.Size(121, 28);
+            this.cbxNome.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Nome Contato";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(205, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Nome:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 170);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Telefone:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(205, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Email:";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(255, 150);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(75, 13);
+            this.lblNome.TabIndex = 17;
+            this.lblNome.Text = "respostaNome";
+            // 
+            // lblTelefone
+            // 
+            this.lblTelefone.AutoSize = true;
+            this.lblTelefone.Location = new System.Drawing.Point(255, 170);
+            this.lblTelefone.Name = "lblTelefone";
+            this.lblTelefone.Size = new System.Drawing.Size(89, 13);
+            this.lblTelefone.TabIndex = 18;
+            this.lblTelefone.Text = "respostaTelefone";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(255, 190);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(72, 13);
+            this.lblEmail.TabIndex = 19;
+            this.lblEmail.Text = "respostaEmail";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(211, 373);
+            this.BackColor = System.Drawing.Color.FloralWhite;
+            this.ClientSize = new System.Drawing.Size(384, 386);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblTelefone);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbxNome);
+            this.Controls.Add(this.rbEmail);
+            this.Controls.Add(this.rbTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txbTelefone);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnLer);
             this.Controls.Add(this.btnSalvar);
-            this.Controls.Add(this.txbInput);
+            this.Controls.Add(this.txbNome);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Agenda";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,9 +255,24 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox txbInput;
+        private System.Windows.Forms.TextBox txbNome;
         private System.Windows.Forms.Button btnSalvar;
         private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbEmail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbTelefone;
+        private System.Windows.Forms.RadioButton rbTelefone;
+        private System.Windows.Forms.RadioButton rbEmail;
+        private System.Windows.Forms.ComboBox cbxNome;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblTelefone;
+        private System.Windows.Forms.Label lblEmail;
     }
 }
 
--- WindowsFormsApp5/WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -19,11 +19,73 @@ namespace WindowsFormsApp5
             InitializeComponent();
         }
 
+        String caminhoDoArquivo = "C:/Users/erick.vgusson/source/repos/WinForms5/WindowsFormsApp5/WindowsFormsApp5/Arquivos/dados.txt";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string texto = LerArquivo(caminhoDoArquivo);
+
+            Array lista = texto.Split('\n');
+
+            foreach (string pessoa in lista)
+            {
+
+                string[] dados = pessoa.Split('|');
+
+                //MessageBox.Show(dados[0]);
+
+                cbxNome.Items.Add(dados[0].ToUpper());
+
+            }
+
+        }
+
+        string LerArquivo(string endereco)
+        {
+            string conteudo = "";
+            if (File.Exists(endereco))
+            {
+
+                using (StreamReader sr = new StreamReader(endereco))
+                {
+
+                    conteudo = sr.ReadToEnd();
+
+                }
+
+            }
+
+            return conteudo;
+
+        }
+
         private void btnSalvar_Click(object sender, EventArgs e)
         {
 
-            string texto = txbInput.Text;
-            String caminhoDoArquivo = "C:/Users/erick.vgusson/source/repos/WinForms5/WindowsFormsApp5/WindowsFormsApp5/Arquivos/dados.txt";
+            string nome = txbNome.Text;
+            string telefone = txbTelefone.Text;
+            string email = txbEmail.Text;
+            string separador = "|";
+
+            if (rbTelefone.Checked)
+            {
+
+                email = "-";
+
+            }
+
+            if (rbEmail.Checked)
+            {
+
+                telefone = "-";
+
+
+            }
+
+
+            string contato = nome + separador + telefone + separador + email;
+
+            string textoAntigo = LerArquivo(caminhoDoArquivo);
 
             //StreamWriter(Caminho do arquivo);
             //StreamReader(caminho do arquivo);
@@ -31,12 +93,35 @@ namespace WindowsFormsApp5
             using (StreamWriter sw = new StreamWriter(caminhoDoArquivo))
             {
 
-                sw.WriteLine(texto);
+                sw.WriteLine(textoAntigo + contato);
 
             }
 
             MessageBox.Show("Arquivo criado com sucesso");
 
         }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+
+            string texto = LerArquivo(caminhoDoArquivo);
+
+            Array lista = texto.Split('\n');
+
+            foreach (string pessoa in lista)
+            {
+
+                string[] dados = pessoa.Split('|');
+
+                cbxNome.Items.Add(dados[0].ToUpper());
+
+                lblNome.Text = dados[0];
+                lblTelefone.Text = dados[1];
+                lblEmail.Text = dados[2];
+
+            }
+
+        }
+
     }
 }
