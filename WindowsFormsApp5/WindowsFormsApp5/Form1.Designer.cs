d5ab013a568e4cc6d46d193db2563a1b2313ddff
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-11-22 08:39:00 -0300

    Base

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbInput = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txbInput
+            // 
+            this.txbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbInput.Location = new System.Drawing.Point(13, 13);
+            this.txbInput.Name = "txbInput";
+            this.txbInput.Size = new System.Drawing.Size(186, 30);
+            this.txbInput.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(12, 323);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(89, 38);
+            this.btnSalvar.TabIndex = 1;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnLer
+            // 
+            this.btnLer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLer.Location = new System.Drawing.Point(108, 323);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(91, 38);
+            this.btnLer.TabIndex = 2;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(211, 373);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txbInput);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Agenda";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbInput;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnLer;
+    }
+}
+
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+
+            string texto = txbInput.Text;
+            String caminhoDoArquivo = "C:/Users/erick.vgusson/source/repos/WinForms5/WindowsFormsApp5/WindowsFormsApp5/Arquivos/dados.txt";
+
+            //StreamWriter(Caminho do arquivo);
+            //StreamReader(caminho do arquivo);
+
+            using (StreamWriter sw = new StreamWriter(caminhoDoArquivo))
+            {
+
+                sw.WriteLine(texto);
+
+            }
+
+            MessageBox.Show("Arquivo criado com sucesso");
+
+        }
+    }
+}
